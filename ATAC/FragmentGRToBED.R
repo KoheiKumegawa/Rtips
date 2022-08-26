9b150e4ae4ec6afdc420ae12ad7bdc312921af46
Kohei Kumegawa <kohei.kumegawa@gmail.com>
2022-08-26 12:07:15 +0900

    Update FragmentGRToBED.R

--- ATAC/FragmentGRToBED.R
+++ ATAC/FragmentGRToBED.R
@@ -1,12 +1,16 @@
+#------------------------------------------------------------------------------
+# FragmentGRToBED.R
+#------------------------------------------------------------------------------
+#last update: 2022-08-26
+#Kohei Kumegawa, Japanese Foundation for Cancer Research
+library(data.table)
 
-
-FragmentGRtoBED <- function(
+FragmentGRToBED <- function(
   gr = NULL,
   name = NULL,
   outputDir = NULL
 ){
   d <- data.frame(seqnames = seqnames(gr), start = start(gr)-1, end = end(gr))
   write.table(d, paste0(outputDir, "/", name, "-fragments.bed"), row.names = F, col.names = F, quote = F, sep = "\t")
-  
   return(NULL)
 }
